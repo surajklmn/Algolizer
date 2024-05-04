3b562bf3fbb5e857a80d198e2938124fe01b6a94
Ashitom <sasukeuchia32828@gmail.com>
2024-05-04 12:24:09 +0545

    Input Data And Then Enqueue

--- src/queue.cpp
+++ src/queue.cpp
@@ -14,58 +14,77 @@ extern screen currentscreen;
 struct Element{ 
     Rectangle dimension;
     Color color;
+    std::string text;
     Vector2 line;
-};
+  };
 
 
 
 void RunQueueVisualizer(){
-    const int box_width = 50;
-    const int box_height = 50;
+    const int box_width = 55;
+    const int box_height = 55;
     const int starting_X = 30;
     const int element_gap = 70;
+    bool dataEntered = false;
     const Color base_color = DARKGRAY;
-    int queue_size;
     InputBox input_widget = InputBox(Rectangle { 800 / 2.0 - 100, 600.0 / 2 - 100, 200, 40 });
     std::vector<Element> queue_item;
-
-    std::vector<Rectangle> Rectangles; 
+ 
+ 
     while(!WindowShouldClose()){
 
      BeginDrawing();
-     ClearBackground(RAYWHITE); 
-     DrawText("Press E To Enqueue Element",30,40,15,GRAY);
+     ClearBackground(RAYWHITE);
+     DrawText("Press Enter To Enqueue Element",30,20,15,GRAY);
+     DrawText("Press Enter To Enqueue Element",30,40,15,GRAY);
      DrawText("Press D To Dequeue Element",30,60,15,GRAY);
-     
-    if(IsKeyPressed(KEY_E)){
-        if(queue_item.empty()){
-            Rectangle rectangle = {starting_X,300,box_width,box_height};
-            Color color = GREEN;
-            queue_item.push_back({rectangle,color});
-        }else{
+    if(IsKeyPressed(KEY_B)){
+        currentscreen = screenStack.top();
+        break;
+    } 
+    if(IsKeyPressed(KEY_ENTER)){
+       input_widget.Update();
+       std::string data;
+       if(!input_widget.IsEnteringInput()){
+            data = std::to_string(input_widget.GetInputValue());
+            dataEntered = true;    
+         }
+        if(dataEntered){
+                if(queue_item.empty()){
+                    Rectangle rectangle = {starting_X,300,box_width,box_height};
+                     Color color = GREEN;
+                     queue_item.push_back({rectangle,color,data});
+                }
+                else{
+
                 Rectangle lastElement = queue_item.back().dimension;
                 Rectangle rectangle = {lastElement.x + element_gap , 300, box_width,box_height};
                 Vector2 line_start = queue_item.back().line;
                 line_start.x = lastElement.width+lastElement.x;
                 line_start.y = lastElement.y+lastElement.height/2;
-                queue_item.push_back({rectangle,base_color,line_start});
+                queue_item.push_back({rectangle,base_color,data,line_start});
                  
             }
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
+        dataEntered = false; 
+        }
+    }
+    if(input_widget.IsEnteringInput()){
+            input_widget.Draw();
     }
     if(IsKeyPressed(KEY_D)){
         if(!queue_item.empty()){
-                 std::this_thread::sleep_for(std::chrono::milliseconds(100));
-                 queue_item.erase(queue_item.begin());
+                              queue_item.erase(queue_item.begin());
                  queue_item.begin()->color = GREEN;
         }  
     }
 
   
     for(int i=0;i<queue_item.size();i++){ 
-        DrawRectangleRec(queue_item[i].dimension, queue_item[i].color);
-        
+         DrawRectangleRec(queue_item[i].dimension, queue_item[i].color);
+         
+       
+
+    
         if(i>0){
             DrawLineEx(queue_item[i].line,{queue_item[i].dimension.x,queue_item[i].dimension.y+(box_height/2.0f)},2.0f, BLACK);
             DrawTriangle( {queue_item[i].dimension.x-5,queue_item[i].dimension.y+(box_height/2.0f)-5}, 
@@ -74,18 +93,23 @@ void RunQueueVisualizer(){
                          {queue_item[i].dimension.x+1,queue_item[i].dimension.y+(box_height/2.0f)}, BLACK);
 
             }
-           if(i>0 && i==queue_item.size()-1){
-                 DrawRectangleRec(queue_item[i].dimension,RED);
+        
+        if(i>0 && i== queue_item.size()-1){
+                    DrawRectangleRec(queue_item[i].dimension, RED);
+
             }
+         DrawText(queue_item[i].text.c_str(),queue_item[i].dimension.x+(box_width/2.0f)-18,
+                 queue_item[i].dimension.y+(box_height/2.0f)-5,20,WHITE);
          
 
     }
-    
-    
+   
+
 
     // Drawing Indicators
 
     if(!queue_item.empty()){
+        
         const Rectangle dimensionS = queue_item.begin()->dimension;
         const Vector2 head_line = {dimensionS.x+(box_width/2.0f),dimensionS.y-30};
         DrawLineEx(head_line,{dimensionS.x+(box_width)/2.0f,dimensionS.y},2.0f,LIME);
@@ -110,7 +134,6 @@ void RunQueueVisualizer(){
 
  
     }  
-
 
 
     EndDrawing();
