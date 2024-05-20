900bfa9792977406f16653dd399182e68cb98046
surajklmn <surajbhattarai339@gmail.com>
2024-05-20 23:45:31 +0545

    Add gap between bars

--- src/selection_sort.cpp
+++ src/selection_sort.cpp
@@ -8,6 +8,7 @@ const int screen_width = 800;
 const int screen_height = 600;
 const int bar_width = 20;
 const int numBars = screen_width/bar_width;
+const int gap = 5;
 extern std::stack<screen> screenStack;
 extern screen currentscreen;
 void swap(int &x, int  &y){
@@ -23,12 +24,12 @@ void selectionVisualizer(int arr[], int n){
             ClearBackground(RAYWHITE);
             for(int k = 0; k < numBars; k++){
                 if(k == i || k == j){
-                    DrawRectangle((k*bar_width) + 2, screen_height - arr[k], bar_width, arr[k], RED);
+                    DrawRectangle(k*(bar_width+gap), screen_height - arr[k], bar_width, arr[k], RED);
 
                 }
                 else{
 
-                    DrawRectangle((k*bar_width)+2,screen_height - arr[k], bar_width, arr[k], BLACK);
+                    DrawRectangle(k*(bar_width+gap),screen_height - arr[k], bar_width, arr[k], BLACK);
                 }
 
             }
@@ -46,11 +47,11 @@ void selectionVisualizer(int arr[], int n){
         ClearBackground(RAYWHITE);
         for(int k = 0; k < numBars; k++){
             if(k == i || k == min_index){
-                DrawRectangle((k * bar_width) + 2, screen_height - arr[k], bar_width,arr[k],GREEN);
+                DrawRectangle(k *(bar_width + gap), screen_height - arr[k], bar_width,arr[k],GREEN);
             }
 
             else{
-                DrawRectangle((k* bar_width) + 2, screen_height - arr[k], bar_width,arr[k], BLACK);
+                DrawRectangle(k*( bar_width + gap), screen_height - arr[k], bar_width,arr[k], BLACK);
             }
         }
         EndDrawing();
@@ -72,6 +73,7 @@ void RunSelectionSortVisualizer(){
         ClearBackground(RAYWHITE);
         if(IsKeyPressed(KEY_B)){
             currentscreen = screenStack.top();
+
             break;
 
         }
@@ -87,7 +89,7 @@ void RunSelectionSortVisualizer(){
 
         int posX = 2;
         for(int i = 0; i < numBars; i++){
-            DrawRectangle(i*bar_width, GetScreenHeight() - data[i], bar_width, data[i], BLACK);
+            DrawRectangle(i*(bar_width+gap), GetScreenHeight() - data[i], bar_width, data[i], BLACK);
 
         }
 
