529aca95ec9e19589f68906f0eefa6c46fda7cd9
surajklmn <surajbhattarai339@gmail.com>
2024-05-20 22:44:00 +0545

    Selection Sort

--- src/selection_sort.cpp
+++ src/selection_sort.cpp
@@ -2,10 +2,14 @@
 #include "raylib.h"
 #include <chrono>
 #include <thread>
+#include <stack>
+#include "selection_sort.h"
 const int screen_width = 800;
 const int screen_height = 600;
 const int bar_width = 20;
 const int numBars = screen_width/bar_width;
+extern std::stack<screen> screenStack;
+extern screen currentscreen;
 void swap(int &x, int  &y){
     int temp = x;
     x = y;
@@ -19,12 +23,12 @@ void selectionVisualizer(int arr[], int n){
             ClearBackground(RAYWHITE);
             for(int k = 0; k < numBars; k++){
                 if(k == i || k == j){
-                    DrawRectangle(k*bar_width, screen_height - arr[k], bar_width, arr[k], RED);
+                    DrawRectangle((k*bar_width) + 2, screen_height - arr[k], bar_width, arr[k], RED);
 
                 }
                 else{
 
-                    DrawRectangle(k*bar_width,screen_height - arr[k], bar_width, arr[k], BLACK);
+                    DrawRectangle((k*bar_width)+2,screen_height - arr[k], bar_width, arr[k], BLACK);
                 }
 
             }
@@ -35,41 +39,56 @@ void selectionVisualizer(int arr[], int n){
 
             }
         }
-       
-        
-         swap(arr[min_index],arr[i]);
+
+
+        swap(arr[min_index],arr[i]);
         BeginDrawing();
         ClearBackground(RAYWHITE);
         for(int k = 0; k < numBars; k++){
             if(k == i || k == min_index){
-                DrawRectangle(k * bar_width, screen_height - arr[k], bar_width,arr[k],GREEN);
+                DrawRectangle((k * bar_width) + 2, screen_height - arr[k], bar_width,arr[k],GREEN);
             }
 
             else{
-                DrawRectangle(k* bar_width, screen_height - arr[k], bar_width,arr[k], BLACK);
+                DrawRectangle((k* bar_width) + 2, screen_height - arr[k], bar_width,arr[k], BLACK);
             }
         }
-            EndDrawing();
-            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        EndDrawing();
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
 }
-int main(void){
+void RunSelectionSortVisualizer(){
 
-    InitWindow(screen_width,screen_height, "Selection Sort");
     SetTargetFPS(60);
     int data[numBars];
     for(int i = 0; i < numBars; i++){
         data[i] = GetRandomValue(10, screen_height-20);
 
     }
+    bool isSorted = false;
+
+    const int bar_gap = bar_width + 1;
     while (!WindowShouldClose()) {
-        if(IsKeyPressed(KEY_SPACE)){
-            selectionVisualizer(data, numBars);
-        }
-        BeginDrawing();
         ClearBackground(RAYWHITE);
+        if(IsKeyPressed(KEY_B)){
+            currentscreen = screenStack.top();
+            break;
+
+        }
+        if(!isSorted){
+            selectionVisualizer(data, numBars);
+            isSorted = true;
+        }
+
+        BeginDrawing();
+
+
+        ClearBackground(RAYWHITE);
+
+        int posX = 2;
         for(int i = 0; i < numBars; i++){
-            DrawRectangle(i * bar_width, screen_height - data[i], bar_width, data[i], BLACK);
+            DrawRectangle(i*bar_width, GetScreenHeight() - data[i], bar_width, data[i], BLACK);
+
         }
 
 
@@ -77,7 +96,6 @@ int main(void){
         EndDrawing(); 
     }
     CloseWindow();
-    return 0;
 
 }
 
