05294735f538de5e37176b450940376419a02e69
Ashitom <sasukeuchia32828@gmail.com>
2024-05-22 09:16:06 +0545

    Implement Insertion and Traversal(Pre-Order)

--- src/tree.cpp
+++ src/tree.cpp
@@ -1,5 +1,37 @@
 #include "tree.hpp"
 
+#include <iostream>
+ 
+void Tree::InsertNode(int data){
+    Node* data_node = new Node(data);
+    if(root_node == nullptr){
+        root_node = data_node; 
+    }
+
+    Node* current_node = root_node;
+    while(true){
+        if(data < current_node->data){
+            if(current_node->left_node == nullptr){
+                current_node->left_node = data_node; 
+                break;
+            }
+            current_node = current_node->left_node;
+        }else{
+            if(current_node->right_node ==nullptr){
+                current_node->right_node = data_node;
+                break;
+            } 
+            current_node = current_node->left_node;
+        }
+    }
+}
 
 
+void Tree::TraversePre(Node* node){
+    if(node == nullptr) return;
 
+    std::cout << node->data;
+    TraversePre(node->left_node); // Left
+    TraversePre(node->right_node);
+
+}
--- src/tree.hpp
+++ src/tree.hpp
@@ -4,18 +4,20 @@ struct Node{
     int data;
     Node* left_node;
     Node* right_node;
+
+    Node(int data): data(data),left_node(nullptr),right_node(nullptr){}
+
 };
 
 class Tree{
 
 public:
-   private:
     Tree(): root_node(nullptr){}
     void InsertNode(int data);
     void Deletion();
-    void TraverseIn();
-    void TraversePre();
-    void TraversePos();
+    void TraverseIn(Node* root_node);
+    void TraversePre(Node* root_node);
+    void TraversePos(Node* root_node);
 
 private:
     Node* root_node;
