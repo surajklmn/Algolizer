b7c6619d3884c48d036454ef6d48d89ae8e59eb0
FrosTx <anmoldahal16@gmail.com>
2024-06-03 20:58:18 +0545

    quicksort visualization added

---
+++ src/quick_sort.cpp
@@ -0,0 +1,92 @@
+#include "quick_sort.hpp"
+#include <chrono>
+#include <raylib.h>
+#include <thread>
+#include <vector>
+#include "main.h"
+#include <stack>
+
+extern std::stack<screen> screenStack;
+extern screen currentscreen;
+extern int bar_width;
+extern int comparision_count;
+
+void swap(int &x, int &y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+void DrawState(const std::vector<int>& arr, int low, int high, int pivot = -1) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    for (int i = 0; i < arr.size(); i++) {
+        if (i == low) {
+            DrawRectangle(i * (bar_width + 1), GetScreenHeight() - arr[i], bar_width, arr[i], YELLOW);
+        } else if (i == high) {
+            DrawRectangle(i * (bar_width + 1), GetScreenHeight() - arr[i], bar_width, arr[i], RED);
+        } else if (i == pivot) {
+            DrawRectangle(i * (bar_width + 1), GetScreenHeight() - arr[i], bar_width, arr[i], GREEN);
+        } else {
+            DrawRectangle(i * (bar_width + 1), GetScreenHeight() - arr[i], bar_width, arr[i], BLACK);
+        }
+    }
+    EndDrawing();
+}
+
+int partition(std::vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        DrawState(arr, low, high, j);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+            DrawState(arr, low, high, j);
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    DrawState(arr, low, high, i + 1);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    return (i + 1);
+}
+
+void quickSort(std::vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void RunQuickSortVisualizer() {
+    SetTargetFPS(30); // Change this value to increase/decrease animation time
+
+    const int max_barHeight = GetScreenHeight() - 100;
+    const int min_barHeight = 20;
+    const int bar_count = GetScreenWidth() / (bar_width + 1);
+
+    std::vector<int> dataset;
+    for (int i = 0; i < bar_count; i++) {
+        dataset.push_back(GetRandomValue(min_barHeight, max_barHeight));
+    }
+    bool isSorted = false;
+
+    while (!WindowShouldClose()) {
+        ClearBackground(RAYWHITE);
+        if (IsKeyPressed(KEY_B)) {
+            currentscreen = screenStack.top();
+            break;
+        }
+        if (!isSorted) {
+            quickSort(dataset, 0, dataset.size() - 1);
+            isSorted = true;
+        }
+        DrawState(dataset, -1, -1);
+    }
+}
