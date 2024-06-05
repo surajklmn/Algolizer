680c5bca64d47a241366141e8939ab6f92bf80c6
Ashitom <sasukeuchia32828@gmail.com>
2024-06-05 13:33:31 +0545

    Reduce Selection Sort Delay

--- src/selection_sort.cpp
+++ src/selection_sort.cpp
@@ -39,7 +39,7 @@ void selectionVisualizer(int arr[], int n){
 
             }
             EndDrawing();
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
             if(arr[j] < arr[min_index] ){
                 min_index = j; 
 
