3e5a2bd90965ae763e47df737b11db97c4aba45d
Ashitom <sasukeuchia32828@gmail.com>
2024-06-07 17:50:42 +0545

    Add Comparision Count For Quick Sort

--- src/quick_sort.cpp
+++ src/quick_sort.cpp
@@ -16,6 +16,9 @@ extern int comparision_count;
 void DrawState(const std::vector<int>& arr, int low, int high, int pivot = -1) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
+    DrawText(TextFormat("Sample Size : %d ",arr.size()),20,20,15,BLACK);
+    DrawText(TextFormat("Comparisions : %d ",comparision_count),20,40,15,BLACK);
+
     for (int i = 0; i < arr.size(); i++) {
         if (i == low) {
             DrawRectangle(i * (bar_width + 1), GetScreenHeight() - arr[i], bar_width, arr[i], YELLOW);
@@ -36,28 +39,32 @@ int partition(std::vector<int>& arr, int low, int high) {
 
     for (int j = low; j <= high - 1; j++) {
         DrawState(arr, low, high, j);
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        comparision_count++; 
         if (arr[j] < pivot) {
             i++;
+           
             std::swap(arr[i], arr[j]);
             DrawState(arr, low, high, j);
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
         }
     }
     std::swap(arr[i + 1], arr[high]);
     DrawState(arr, low, high, i + 1);
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
     return (i + 1);
 }
 
 void quickSort(std::vector<int>& arr, int low, int high) {
+    comparision_count++;
     if (low < high) {
+       
         int pi = partition(arr, low, high);
 
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
+
 }
 
 void RunQuickSortVisualizer() {
@@ -66,7 +73,7 @@ void RunQuickSortVisualizer() {
     const int max_barHeight = GetScreenHeight() - 100;
     const int min_barHeight = 20;
     const int bar_count = GetScreenWidth() / (bar_width + 1);
-
+    comparision_count = 0;
     std::vector<int> dataset;
     for (int i = 0; i < bar_count; i++) {
         dataset.push_back(GetRandomValue(min_barHeight, max_barHeight));
